eb84011fb51423fe51aa73a94cb463d6d2851300
spry <yoshida@spr.jp>
2016-04-14 07:21:36 +0900

    Add Model

--- src/MvcMovie/Models/ApplicationDbContext.cs
+++ src/MvcMovie/Models/ApplicationDbContext.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.Data.Entity;
+using MvcMovie.Models;
 
 namespace MvcMovie.Models
 {
@@ -16,5 +17,6 @@ namespace MvcMovie.Models
             // For example, you can rename the ASP.NET Identity table names and more.
             // Add your customizations after calling base.OnModelCreating(builder);
         }
+        public DbSet<Movie> Movie { get; set; }
     }
 }
